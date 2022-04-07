6d333de25287c718936a2a2c528fce44bc5caf67
yoonlight <yoonlight12@gmail.com>
2022-04-07 22:36:23 +0900

    style: add equal blank

--- src/__main__.py
+++ src/__main__.py
@@ -13,7 +13,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 user_dim = num_words_dict["user_id"]
 item_dim = num_words_dict["movie_id"]
 
-inputs=[layers.Input((1, )), layers.Input((1, ))]
+inputs = [layers.Input((1, )), layers.Input((1, ))]
 
 model = MF(user_dim=user_dim, item_dim=item_dim)
 model.compile(optimizer="SGD", loss="binary_crossentropy", metrics=[
